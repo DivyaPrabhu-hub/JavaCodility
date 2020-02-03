0d951f9470cef7ecf6c9a983567d3ec5bdd9b9f2
divya <divya@10.0.0.103>
2020-02-03 08:56:25 -0800

    Java assignment Exam

---
+++ Java Codility Test/src/JavaCodilityTest/Assignment1.java
@@ -0,0 +1,23 @@
+package JavaCodilityTest;
+
+import java.util.TreeMap;
+
+public class Assignment1 {
+	/*
+	 * 1. Write a java program to store 5 student information in a treeMapNote: student object has student name and student reg no
+	 */
+
+	public static void main(String[] args) {
+		
+		TreeMap st = new TreeMap ();
+		st .put( "1001" , "divya" ) ;
+		st .put( "1002" , "vidya" ) ;
+		st .put( "1003" , "deepa" ) ;
+		st .put( "1004" , "rishi" ) ;
+		st .put( "1005" , "ria" ) ;
+		System. out .println( st );
+		
+
+	}
+
+}
